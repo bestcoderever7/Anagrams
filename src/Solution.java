1ebf6fc32300a792c9637a890aefe10cacd15a66
TJHL <league@WTS7.attlocal.net>
2016-10-16 14:36:23 -0700

    s

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,34 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
 public class Solution {
 
 	static boolean isAnagram(String firstString, String secondString) {
-		int letters = secondString.length();
 		firstString = firstString.toLowerCase();
 		secondString = secondString.toLowerCase();
-		for (int i = 0; i < firstString.length(); i++) {
-			for (int j = 0; j < secondString.length(); j++) {
-				if (firstString.charAt(i) == secondString.charAt(j)) {
-					letters--;
-					secondString = secondString.replaceFirst(Character.toString(secondString.charAt(j)), "");
+		boolean areAnagrams = true;
+		char[] letters1 = firstString.toCharArray();
+		char[] letters2 = secondString.toCharArray();
+		Arrays.sort(letters1);
+		Arrays.sort(letters2);
+		for(char c : letters1)
+		{
+			for(char c2 : letters2)
+			{
+				if(Character.toString(c).equals(Character.toString(c2))){}
+				else
+				{
+					areAnagrams = false;
 					break;
 				}
 			}
 		}
-		if (letters == 0)
-			return true;
-		else
-			return false;
-		// Complete the function
-
+		return areAnagrams;
 	}
 
 	public static void main(String[] args) {
-
 		String firstString = JOptionPane.showInputDialog("Enter a word.");
 		String secondString = JOptionPane
 				.showInputDialog("Enter another word.");
